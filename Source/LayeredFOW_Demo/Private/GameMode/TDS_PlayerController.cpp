0de9ab6204a414f15e7322896bc8f59533057c14
Enzo Resse <enzo.resse@epitech.eu>
2024-04-04 15:58:56 +0200

    [PROG][ADD] Add movement controls

---
+++ Source/LayeredFOW_Demo/Private/GameMode/TDS_PlayerController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/TDS_PlayerController.h"
+
+ATDS_PlayerController::ATDS_PlayerController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+}
---
+++ Source/LayeredFOW_Demo/Public/GameMode/TDS_PlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TDS_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAYEREDFOW_DEMO_API ATDS_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+
+	ATDS_PlayerController();
+};
